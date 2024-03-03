b28be4a95bdf597744c3f312fa7a385692a125e4
juannmachado1995 <juannmachado1995@gmail.com>
2024-03-03 02:24:06 -0300

    cards2

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbarapp from './components/navbar/Navbarapp'
 import ItemListContainer from './components/itemListContainer/ItemListContainer'
-import ItemCount from './components/itemCount/itemCount'
-import Promises from './components/promises/promises'
-import PracticandoApi from './components/PRACTICANDO/PracticandoApi'
-import AgregarProducto from './components/PRACTICANDO/PracticandoApi'
+
 
 function App() {
 
